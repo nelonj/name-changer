012213a4e90185e102d1ba38d4049f310f1a6d63
Esme Hotston Moore <ehotstonmoore@hotmail.co.uk>
2021-02-16 15:46:52 +0000

    for olivia and india

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
+  let name = "Esme"
+  console.log("I have rendered!")
+  console.log("name", name)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Name changer!
       </header>
+      <div>
+        Name: {name}
+      </div>
+      <div>
+        Change name to:
+        <button onClick={() => name = "Esme"}>Esme</button>
+        <button onClick={() => name = "Olivia"}>Olivia</button>
+        <button onClick={() => name = "India"}>India</button>
+      </div>
+
     </div>
+
   );
 }
 

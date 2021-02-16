1dee7f969ed10396fd7f8aa2f71342bb0e09846f
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-16 18:58:19 +0100

    the app re-renders because we involved states, allowing the heading to update every time a button is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-
+import React, {useState} from 'react'
 
 import './App.css';
 
+interface NameFieldprops {
+  nametoshow: string,
+
+}
+
+function NameField (props: NameFieldprops) {
+  console.log(props)
+  return (
+    <div> Name: {props.nametoshow} </div>
+  )
+}
+
+interface ButtonFieldprops {
+  name: string,
+  clickHandler: () => void
+}
+function Button (props: ButtonFieldprops) {
+  return (
+  <button
+    onClick = {props.clickHandler} > 
+    {props.name}
+  </button> )
+}
+
+const names = ['Esme', 'Olivia', 'India']
+
 function App() {
-  let name = "Esme"
+  const [name, setName] = useState("Esme")
+  // let name = "Esme"
   console.log("I have rendered!")
   console.log("name", name)
   return (
     <div className="App">
+      
       <header className="App-header">
         Name changer!
       </header>
-      <div>
-        Name: {name}
-      </div>
+
+     <NameField nametoshow = {name} />   
+      
       <div>
         Change name to:
-        <button onClick={() => name = "Esme"}>Esme</button>
-        <button onClick={() => name = "Olivia"}>Olivia</button>
-        <button onClick={() => name = "India"}>India</button>
+        
+        <button onClick={() => setName("Esme")}> Esme </button>
+        <button onClick={() => setName("Olivia")}> Olivia </button>
+        <button onClick={() => setName("India")}> India</button>
       </div>
 
     </div>
